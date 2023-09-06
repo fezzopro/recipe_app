21614f6daceb4dfe8feaa557e542385e2a3f745f
Fezzopro <fezzopro@gmail.com>
2023-09-06 14:20:43 +0200

    Correct Recipe model typo

--- db/migrate/20230906101358_create_recipes.rb
+++ db/migrate/20230906101358_create_recipes.rb
@@ -2,7 +2,7 @@ class CreateRecipes < ActiveRecord::Migration[7.0]
   def change
     create_table :recipes do |t|
       t.string :name
-      t.time :prearation_time
+      t.time :preparation_time
       t.time :cooking_time
       t.text :description
       t.boolean :public
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_06_060430) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_06_101358) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -25,16 +25,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_06_060430) do
     t.index ["user_id"], name: "index_foods_on_user_id"
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  add_foreign_key "foods", "users"
   create_table "recipes", force: :cascade do |t|
     t.string "name"
-    t.time "prearation_time"
+    t.time "preparation_time"
     t.time "cooking_time"
     t.text "description"
     t.boolean "public"
@@ -50,5 +43,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_06_060430) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "foods", "users"
   add_foreign_key "recipes", "users"
 end
