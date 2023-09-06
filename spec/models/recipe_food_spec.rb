7ce4f72511edc45d52bb6e28efe00733054e73b8
Fezzopro <fezzopro@gmail.com>
2023-09-06 16:30:11 +0200

    Add Recipe and Recipe_Food Model specs

---
+++ spec/models/recipe_food_spec.rb
@@ -0,0 +1,34 @@
+require 'rails_helper'
+
+RSpec.describe RecipeFood, type: :model do
+  before :each do
+    @user = User.create(name: 'User 1')
+    @food = Food.create(name: 'Food 1', measurement_unit: '1', price: '23.1', quantity: 1, user: @user)
+    @recipe = Recipe.new(name: 'Recipe 1', preparation_time: 'time', cooking_time: 'time',
+                         description: 'Recipe 1 description', public: true, user: @user)
+  end
+
+  describe 'Model Association' do
+    it { should belong_to(:recipe) }
+    it { should belong_to(:food) }
+  end
+
+  describe 'Model Attributes' do
+    it { should respond_to(:quantity) }
+  end
+
+  describe 'Model Behavior' do
+    it 'is valid with valid attributes' do
+      expect(RecipeFood.new(quantity: 1.2, recipe: @recipe, food: @food)).to be_valid
+    end
+    it 'is not valid without quantity' do
+      expect(RecipeFood.new).to_not be_valid
+    end
+    it 'is not valid without a Recipe' do
+      expect(RecipeFood.new(quantity: 1.2)).to_not be_valid
+    end
+    it 'is not valid without a food' do
+      expect(RecipeFood.new(quantity: 1.2, recipe: @recipe)).to_not be_valid
+    end
+  end
+end
--- spec/models/recipe_spec.rb
+++ spec/models/recipe_spec.rb
@@ -1,10 +1,43 @@
 require 'rails_helper'
 
 RSpec.describe Recipe, type: :model do
+  before :each do
+    @user = User.create(name: 'User 1')
+    @food = Food.create(name: 'Food 1', measurement_unit: '1', price: '23.1', quantity: 1, user: @user)
+  end
+
   describe 'Model Validations' do
     it { should belong_to(:user) }
     it { should validate_presence_of(:name) }
     it { should validate_presence_of(:description) }
     it { should validate_presence_of(:public) }
+  end
+
+  describe 'Model Behavior' do
+    it 'is not valid without a title' do
+      expect(Recipe.new(name: 'Recipe 1', preparation_time: 'time', cooking_time: 'time',
+                        description: 'Recipe 1 description', public: true, user: @user)).to be_valid
+    end
+
+    it 'is not valid without a name' do
+      expect(Recipe.new(preparation_time: 'time', cooking_time: 'time',
+                        description: 'Recipe 1 description', public: true, user: @user)).to_not be_valid
+    end
+    it 'is not valid without a description' do
+      expect(Recipe.new(name: 'Recipe 1', preparation_time: 'time', cooking_time: 'time',
+                        public: true, user: @user)).to_not be_valid
+    end
+    it 'is not valid without a user' do
+      expect(Recipe.new(name: 'Recipe 1', preparation_time: 'time', cooking_time: 'time',
+                        description: 'Recipe 1 description', public: true)).to_not be_valid
+    end
+    it 'is valid without a preparation_time' do
+      expect(Recipe.new(name: 'Recipe 1', cooking_time: 'time',
+                        description: 'Recipe 1 description', public: true, user: @user)).to be_valid
+    end
+    it 'is not valid without a cooking_time' do
+      expect(Recipe.new(name: 'Recipe 1',
+                        description: 'Recipe 1 description', public: true, user: @user)).to be_valid
+    end
   end
 end
