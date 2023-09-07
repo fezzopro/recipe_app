432984109ca34eec842796c66804e2c63f435792
paul-tes <pauasca8462@gmail.com>
2023-09-07 11:57:15 +0300

    Fix rubocop

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,11 @@
 class User < ApplicationRecord
   after_create :save_to_database
-  
+
   has_many :foods
   validates :name, presence: true
 
   private
+
   def save_to_database
     save
   end
