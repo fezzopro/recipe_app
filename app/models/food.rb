17f3dee50ff0777f868bdff30db5c50b69fcdf27
paul-tes <pauasca8462@gmail.com>
2023-09-07 13:58:05 +0300

    Add edit view

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,9 +1,9 @@
 class Food < ApplicationRecord
   after_create :save_to_database
-  # belongs_to :user
-  # validates :name, presence: true
-  # validates :price, presence: true, numericality: { greater_than_or_equal_to: 0 }
-  # validates :quantity, presence: true, numericality: { greater_than_or_equal_to: 0 }
+  belongs_to :user
+  validates :name, presence: true
+  validates :price, presence: true, numericality: { greater_than_or_equal_to: 0 }
+  validates :quantity, presence: true, numericality: { greater_than_or_equal_to: 0 }
 
   private
 
