236651d2a6c4f8471a851bf01726ac2b5507d526
paul-tes <pauasca8462@gmail.com>
2023-09-08 23:04:09 +0300

    Fix rubocop

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,7 +3,5 @@ class HomeController < ApplicationController
     redirect_to foods_path if user_signed_in?
   end
 
-  def show
-    
-  end
+  def show; end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -17,7 +17,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_08_155323) do
   create_table "foods", force: :cascade do |t|
     t.string "name"
     t.string "measurement_unit"
-    t.float "price"
+    t.decimal "price"
     t.integer "quantity"
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
@@ -37,8 +37,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_08_155323) do
 
   create_table "recipes", force: :cascade do |t|
     t.string "name"
-    t.string "preparation_time"
-    t.string "cooking_time"
+    t.time "preparation_time"
+    t.time "cooking_time"
     t.text "description"
     t.boolean "public"
     t.bigint "user_id", null: false
