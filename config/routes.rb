4c00a037659f7c85862b8c3f721c6ab6a5ee7cf1
Fezzopro <fezzopro@gmail.com>
2023-09-08 16:07:32 +0200

    Add recipe_foods RESful routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   
-  resources :recipe
+  resources :recipes
+  resources :recipe_foods
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
