0074679ce746c67c45ae453a8cc20db7fd2a9575
paul-tes <pauasca8462@gmail.com>
2023-09-08 20:40:54 +0300

    Add request test for edit food page

--- spec/requests/foods_spec.rb
+++ spec/requests/foods_spec.rb
@@ -26,4 +26,24 @@ RSpec.describe 'Foods', type: :request do
       expect(response).to have_http_status(:success)
     end
   end
+
+  # describe 'POST /create' do
+  #   it 'creates a new food' do
+  #     expect do
+  #       post foods_path,
+  #            params: { food: { name: 'New Food', measurement_unit: '2', price: '12.3', quantity: 2,
+  #                              user_id: @user.id } }
+  #     end.to change(Food, :count).by(1)
+
+  #     expect(response).to have_http_status(:redirect)
+  #     expect(response).to redirect_to(food_path(Food.last))
+  #     expect(flash[:notice]).to eq('Food was successfully created.')
+  #   end
+  # end
+  describe 'GET /edit' do
+    it 'returns http success' do
+      get edit_food_path(@food)
+      expect(response).to have_http_status(:success)
+    end
+  end
 end
