499cc9bdb59b0a3bee3e12160e65de4fa261cbd9
paul-tes <pauasca8462@gmail.com>
2023-09-09 09:55:49 +0300

    Add public_recipes view

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,26 @@
 Rails.application.routes.draw do
-  root to: 'home#index'
+  get '/public_recipes', to: 'recipes#public_recipes'
+  get 'about/index'
+  get '/general_shopping_list/:id', to: 'shopping_list#index', as: 'general_shopping_list'
   devise_for :users
-  resources :recipes
-  resources :recipe_foods
-  get 'public_recipes' => 'home#index'
-  get 'public_recipes/:id' => 'home#show'
-
+  devise_scope :user do
+    authenticated :user do
+      root :to => 'recipes#public_recipes', as: :authenticated_root
+    end
+    unauthenticated :user do
+      root :to => 'recipes#public_recipes'
+    end
+  end
+  resources :foods
+  resources :recipes do
+    resources :recipe_foods
+    collection do 
+      get 'public_recipes', to: 'recipes#public_recipes'
+    end
+  end
+  resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
-  resources :foods
-  resources :users
 end
